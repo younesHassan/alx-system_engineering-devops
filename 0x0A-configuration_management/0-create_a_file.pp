32473fdc56a8a1a2d66b09ddb12bda3448ad62d1
younesHassan <105860222+younesHassan@users.noreply.github.com>
2023-03-25 01:18:59 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Creates a /tmp file
-file { '/tmp/holberton':
-  path    => '/tmp/holberton',
+# creates a file in /tmp
+
+file { '/tmp/school':
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
